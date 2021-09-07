33c61355fdf4f2ba85501a575c29994dad815929
Donelle Harris <donelle.harris@gmail.com>
2021-09-06 20:10:22 -0500

    Initialize Todo App frontend with Svelte

---
+++ SvelteTodoApp/src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Todo from "./Todo.svelte";
+	import { onMount } from 'svelte';
+    import axios from 'axios';
+    
+    let isError = null;
+    let todos = [];
+	export let name;
+
+</script>
+
+<main>
+	<Todo />
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+  </style>
